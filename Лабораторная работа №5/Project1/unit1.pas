78d8928eb2666269bd4cc1ceac765fd9a88a3ea5
Дмитрий Овчеренко <dmitru400@gmail.com>
2022-11-11 14:17:07 +0300

    Готова ЛР №5

---
+++ Лабораторная работа №5/Project1/unit1.pas
@@ -0,0 +1,62 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+var factW,optW,Rost,Delta:Real;
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  try factW:=StrToFloat(Edit1.Text) except
+      ShowMessage('Вес введен неверно');
+      Form1.ActiveControl:=Edit1;
+      Exit;
+  end;
+
+  try Rost:=StrToFloat(Edit2.Text) except
+      ShowMessage('Рост введен неверно');
+      Form1.ActiveControl:=Edit2;
+      Exit;
+  end;
+
+  optW:=Rost-100;
+  Delta:=abs(factW-optW);
+
+  if optW=factW then Label3.Caption:='Ваш вес идеален';
+  if optW>factW then Label3.Caption:='Вам надо поправиться на '+ FloatToStr(Delta)+ ' кг.'
+  else Label3.Caption:='Вам надо похудеть на '+ FloatToStr(Delta)+ ' кг.';
+
+
+end;
+
+end.
+
